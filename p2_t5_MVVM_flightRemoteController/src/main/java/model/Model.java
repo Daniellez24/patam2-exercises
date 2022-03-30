feef08513d0200c94dd4f65edf4b64b318aa4a23
Daniellez24 <daniellez1123@gmail.com>
2022-03-30 13:54:38 +0300

    set t5_MVVM

---
+++ p2_t5_MVVM_flightRemoteController/src/main/java/model/Model.java
@@ -0,0 +1,93 @@
+package model;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Observable;
+
+// the model connects with the flight simulator
+// the model will read the properties from the properties.txt file
+public class Model extends Observable {
+
+    // <key=what part to change(for example, aileron), value=the value we want to change it to>
+    HashMap<String, String> properties;
+    Socket fg;
+    // in order to write to the socket, we'll have the sockets's outputstream
+    PrintWriter out2fg;
+
+    // the controller will tell the Model from where to take the properties
+    public Model(String propertiesFileName){
+        properties = new HashMap<>();
+        try {
+            // read the properties file, and set the map
+            BufferedReader in = new BufferedReader(new FileReader(propertiesFileName));
+            String line;
+            while((line=in.readLine()) != null){
+                // split the lines from the properties file by ","
+                String sp[] = line.split(",");
+                // insert values into the map
+                properties.put(sp[0],sp[1]);
+            }
+            in.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // connecting to the flight simulator(FlightGear) as Client socket
+        int port = Integer.parseInt(properties.get("port"));
+        try {
+            fg = new Socket(properties.get("ip"), port);
+            out2fg = new PrintWriter(fg.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* from our the properties.txt file:
+    aileron,set /controls/flight/aileron
+    elevators,set /controls/flight/elevator
+    rudder,set /controls/flight/rudder
+    throttle,set /controls/engines/current-engine/throttle
+     */
+
+    // the controller will give us the x, after he takes it from the GUI (view)
+
+    public void setAileron(double x){
+        // we'll get the command: set /controls/flight/aileron, and write it to the socket's outPutStream with the given x
+        String command = properties.get("aileron");
+        out2fg.println(command+" "+x);
+        out2fg.flush();
+    }
+
+    public void setElevators(double x){
+        String command = properties.get("elevators");
+        out2fg.println(command+" "+x);
+        out2fg.flush();
+    }
+
+    public void setRudder(double x){
+        String command = properties.get("rudder");
+        out2fg.println(command+" "+x);
+        out2fg.flush();
+    }
+
+    public void setThrottle(double x){
+        String command = properties.get("throttle");
+        out2fg.println(command+" "+x);
+        out2fg.flush();
+    }
+
+    @Override
+    public void finalize(){
+        // close the socket fg, and the out2fg outPutStream
+        try {
+            out2fg.close();
+            fg.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ p2_t5_MVVM_flightRemoteController/src/main/java/view/WindowController.java
@@ -0,0 +1,96 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+
+import java.util.Observable;
+
+// the view paints the virtual Joystick
+public class WindowController extends Observable {
+
+    @FXML
+    Canvas joystick;
+    // mouse clicked or released
+    boolean mousePushed;
+    // to show mouse move on joystick
+    double jx,jy;
+    // middle of canvas x,y
+    double mx, my;
+
+    double aileron, elevator;
+
+    public WindowController(){
+        mousePushed = false;
+        jx = 0;
+        jy = 0;
+        aileron = 0;
+        elevator = 0;
+    }
+
+    public double getAileron() {
+        return aileron;
+    }
+
+    public double getElevator() {
+        return elevator;
+    }
+
+    void paint(){
+        // in order to paint the joystick, we first need to extract from it a GraphicsContext
+        GraphicsContext gc = joystick.getGraphicsContext2D();
+        // get the middle of the canvas (x,y values of the canvas' center)
+        mx = joystick.getWidth()/2;
+        my = joystick.getHeight()/2;
+        // clear the last drawing everytime "paint()" is called, and then re-paint
+        gc.clearRect(0,0, joystick.getWidth(), joystick.getHeight());
+
+        // paint an oval
+        gc.strokeOval(jx-50, jy-50, 100,100);
+
+        /* get the aileron and elevator's position between -1,1.
+        ATTENTION! here we didn't fix the problem that we can get the mouse out of the canvas,
+        and we might get values bigger/smaller than 1,-1.
+        this can be fixed by taking the max/min value between 1,-1 and the (jx-mx)/mx and (jy-my)/my.
+        the aileron moves on the X's, and the elevator on the Y's */
+        aileron = (jx-mx)/mx;
+        elevator = (jy-my)/my;
+        // notify changes to the controller (the observer)
+        setChanged();
+        notifyObservers();
+        System.out.println(aileron+","+elevator);
+
+    }
+
+    public void mouseDown(MouseEvent me){
+        if(!mousePushed){
+            mousePushed = true;
+            System.out.println("mouse is down");
+        }
+    }
+
+    // release the mouse click
+    public void mouseUp(MouseEvent me){
+        if(mousePushed){
+            mousePushed = false;
+            System.out.println("mouse is up");
+            // return the joystick to the middle on mouse up
+            jx = mx;
+            jy = my;
+            paint();
+        }
+    }
+
+    public void mouseMove(MouseEvent me){
+        // check if there was a mousePush, beacuse we want to relate only to the mouse drag
+        if(mousePushed){
+//            System.out.println("mouse move "+me.getX()+","+me.getY());
+            // paint the joystick according to where to mouse is moving
+            jx = me.getX();
+            jy = me.getY();
+            paint();
+        }
+    }
+
+}
